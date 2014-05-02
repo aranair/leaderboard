67c6fa36ca827bdf10fb5dfb3eeb6fd478e341b0
aranair <boa.homan@gmail.com>
2014-05-02 18:26:45 +0800

    update points

--- client/leaderboard.js
+++ client/leaderboard.js
@@ -8,6 +8,23 @@ if (Meteor.isClient) {
   Template.playerslist.players = function() {
     return Players.find({}, {sort: {score: -1}})
   };
+
+  Template.player.events = {
+    'click .player-controls__plus': function(event) {
+      Meteor.call('plusPoint', {
+        // player id
+      }, function(e, p) {
+        if (!p) { $('.js-create-alert').show(); }
+      });
+    },
+    'click .player-controls__minus': function(event) {
+      Meteor.call('minusPoint', {
+        // player id
+      }, function(e, p) {
+        if (!p) { $('.js-create-alert').show(); }
+      });
+    }
+  }
   
   Template.newplayerform.events = {
     'click .form__save-btn': function(event) {
